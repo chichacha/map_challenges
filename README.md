ee8ae81be6d3fa37f317af7d2ae08e55aa3308b3
Chisato <chisatini@gmail.com>
2019-11-02 20:53:24 -0700

    Day2 Lines - Vancouver street vs avenue

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# map_challenges
+# 30 days Map Challenges 
+
+<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Announcing <a href="https://twitter.com/hashtag/30DayMapChallenge?src=hash&amp;ref_src=twsrc%5Etfw">#30DayMapChallenge</a> in November 2019! Create a map each day of the month with the following themes 🌍🌎🌏<br><br>No restriction on tools. All maps should be created by you. Doing less than 30 maps is fine. <a href="https://twitter.com/hashtag/gischat?src=hash&amp;ref_src=twsrc%5Etfw">#gischat</a> <a href="https://twitter.com/hashtag/geography?src=hash&amp;ref_src=twsrc%5Etfw">#geography</a> <a href="https://twitter.com/hashtag/cartography?src=hash&amp;ref_src=twsrc%5Etfw">#cartography</a> <a href="https://twitter.com/hashtag/dataviz?src=hash&amp;ref_src=twsrc%5Etfw">#dataviz</a> <a href="https://t.co/6Go4VFWcJB">pic.twitter.com/6Go4VFWcJB</a></p>&mdash; Topi Tjukanov (@tjukanov) <a href="https://twitter.com/tjukanov/status/1187713840550744066?ref_src=twsrc%5Etfw">October 25, 2019</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+## List of Challenges
+
+| day|challenge         |
+|---:|:-----------------|
+|   1|Points            |
+|   2|Lines             |
+|   3|Polygons          |
+|   4|Hexagons(!)       |
+|   5|Raster            |
+|   6|Blue              |
+|   7|Red               |
+|   8|Green             |
+|   9|Yellow            |
+|  10|Black and white   |
+|  11|Elevation         |
+|  12|Movement          |
+|  13|Tracks            |
+|  14|Boundaries        |
+|  15|Names             |
+|  16|Places            |
+|  17|Zones            |
+|  18|Globe             |
+|  19|Urban             |
+|  20|Rural             |
+|  21|Environment       |
+|  22|Built environment |
+|  23|Population        |
+|  24|Statistics        |
+|  25|Climate           |
+|  26|Hydrology         |
+|  27|Resources         |
+|  28|Funny             |
+|  29|Experimental      |
+|  30|Home              |
---
+++ day_02/Day2_Lines.R
@@ -0,0 +1,160 @@
+## Day 2 - Lines 
+
+library(osmdata)
+library(tidyverse)
+library(visdat)
+library(skimr)
+library(ggthemes)
+library(janitor)
+
+
+## Get Open Street Data as First Layer
+van_bbox <- c(-123.27,49.195,-123.020,49.315)
+van <- opq(bbox=van_bbox) %>% ## bild an overpass query
+  add_osm_feature(key="highway") %>%  ## highway for street data 
+  osmdata_sf() %>%
+  osm_poly2line() ## convert polygon shaped highway to line
+
+## I missed Day 1 - Point Assignment...
+bus_stop <-van$osm_points %>%
+  filter(highway=="bus_stop") 
+
+bus_stop_skim <-bus_stop %>% as_tibble() %>% skim()
+bus_stop_skim %>% filter(stat=="missing") %>% filter(value<1992) %>% arrange(value) %>%
+  DT::datatable()
+
+bus_stop <-bus_stop %>% 
+  select(osm_id, highway, name, geometry, ref, public_transport, shelter, wheelchair)
+
+bus_stop %>% plot()
+
+
+## Streets are lines! :)
+van_street <- van$osm_lines
+
+
+### Exploring.....  
+## osm_id, geometry, highway seems to have data.
+## only 67% of rows don't have name, but 7912 data point with name.
+## 5776 row has lanes, 5558 surface, 4487 source, 3854 oneway, 3168 maxspeed, 2882 service
+
+tmp_skimmed <-van_street %>% as_tibble() %>% skim()
+
+
+tmp_skimmed %>%
+  filter(stat=="missing") %>%
+  arrange(value) %>%
+  select(variable, missing=value) %>%
+  mutate(missingness=round(missing/nrow(van_street),2),
+         hasdata=nrow(van_street)-missing) %>%
+  left_join(tmp_skimmed %>% filter(stat=="n_unique") %>% select(variable, n_unique=value)) %>%
+  DT::datatable()
+
+var_to_use <- tmp_skimmed %>% 
+  filter(stat=="missing") %>% 
+  top_n(n=10, wt=-value) %>%
+  pull(variable)
+
+van_street <-van_street %>% 
+  select_at(vars(var_to_use))
+
+van_street %>% select_at(vars(var_to_use[c(3:6)])) %>% plot()
+
+van_street %>% as_tibble() %>% 
+  mutate(name_exist=!is.na(name),
+         highway=fct_infreq(highway)) %>%
+  count(highway, name_exist) %>%
+  ggplot(aes(x=highway, y=name_exist)) +
+  geom_tile(aes(fill=n)) +
+  geom_text(aes(label=n), color="white") +
+  coord_flip() +
+  scale_fill_viridis_c()
+
+van_street_name <- van_street %>%
+  filter(!is.na(name) & !is.na(highway))
+
+van_street_name %>% 
+  count(name, sort=T) %>%
+  ggplot() +
+  geom_sf(aes(color=str_sub(name,1L,1L))) +
+  geom_sf(data=bus_stop, color="tomato", size=0.5) +
+  coord_sf(xlim=van_bbox[c(1,3)], ylim=van_bbox[c(2,4)]) +
+  theme_map()
+
+
+van_street_df <-van_street_name %>%
+  mutate(highway=fct_infreq(highway)) %>%
+  group_by(name) %>%
+  summarise(highway_list = paste(unique(highway), collapse="|"),
+            highway_type=n_distinct(highway),
+            n = n(),
+            resid_or_major = sum(highway %in% c("residential","primary","secondary","tertiary")))
+
+van_street_df %>% 
+  ggplot() +
+  geom_sf(aes(color=highway_type)) +
+  coord_sf(xlim=van_bbox[c(1,3)], ylim=van_bbox[c(2,4)]) +
+  theme_map() +
+  scale_color_viridis_c(end=0.8)
+
+
+  
+
+st_suffix <- str_to_title(c("street","avenue","drive","place", "road","crescent","lane","court","way","mews","boulevard","highway","bridge"))
+
+st_direction <-str_to_title(c("west","east","south","north"))
+
+van_street_df <- van_street_df %>%
+  ungroup() %>%
+  mutate(street_type=replace_na(str_extract(name,str_c(st_suffix,collapse="|")),""),
+         direction = replace_na(str_extract(name,str_c(st_direction,collapse="|")),""),
+         num_street =replace_na(as.numeric(str_extract(name,"\\d+")),0))
+
+
+library(tidytext)
+wordcloud_df <- van_street_df %>%
+  as_tibble() %>%
+  select(name, street_type) %>%
+  unnest_tokens(word, name) %>%
+  count(word,street_type, sort=T) %>% 
+  mutate(n=log(n))  %>%
+  mutate(color=case_when(street_type=="Avenue" ~ col_pal[1],
+                         street_type=="Drive" ~ col_pal[4],
+                         street_type=="Lane" ~ col_pal[7],
+                         street_type=="Street" ~ col_pal[10],
+                         TRUE ~ col_pal[13]))
+
+
+
+wordcloud_df %>% select(word, n) %>% 
+  wordcloud2::wordcloud2(rotateRatio=0, 
+                         color=wordcloud_df$color,
+                         fontFamily="Roboto Condensed",
+                         backgroundColor="gray8") 
+
+col_pal <- c("#F44336", "#E91E63", "#9C27B0", "#673AB7", "#3F51B5", "#2196F3", "#03A9F4", "#00BCD4", "#009688", "#4CAF50", "#8BC34A", "#CDDC39", "#FFEB3B", "#FFC107", "#FF9800", "#FF5722", "#795548", "#9E9E9E", "#607D8B") 
+
+col_pal %>% scales::show_col()
+
+van_street_df %>% count(street_type, sort=T)
+
+
+van_street_df %>%
+  mutate(street_type=(fct_other(street_type,
+                                keep=c("Street","Avenue","Drive","Lane")))) %>%
+  ggplot() +
+  geom_sf(data=van_street, color="#ffffff80", size=0.1) +
+  geom_sf(aes(color=street_type), size=0.8) +
+  scale_color_manual(values=col_pal[c(1,4,7,10,13,16)], 
+                     name="street type", guide="none") +
+  coord_sf(xlim=van_bbox[c(1,3)], ylim=van_bbox[c(2,4)]) +
+  theme_map() +
+  theme(plot.background=element_rect(fill="gray8")) 
+
+ggsave("day_02/output/Vancouver_Street_Map.png", width=16, height=12)
+
+
+
+
+
+
